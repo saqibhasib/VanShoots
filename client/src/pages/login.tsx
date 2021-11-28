685dd6e13838e21f01bf1b6d8231d3a19b85b318
kwokyeec <61216008+kwokyeec@users.noreply.github.com>
2021-11-27 16:01:59 -0800

    add css file

---
+++ client/src/pages/login.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import "./login.css";
+import { Col, Row, Container, Form, Button } from "react-bootstrap";
+
+function Login() {
+  return (
+    <div className="login">
+      <Container>
+        <Row>
+          <Col>
+            <Form>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Enter email"
+                  id="email"
+                />
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Password"
+                  id="password"
+                />
+              </Form.Group>
+
+              <Button variant="primary" type="submit">
+                Login
+              </Button>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+export default Login;
